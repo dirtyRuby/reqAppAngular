59b9fa23ab116d7b83ed57ad484802da63d14c51
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-03-10 17:10:25 +0200

    Assets fix.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module ReqAppAngular
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+    config.serve_static_assets = true
   end
 end
