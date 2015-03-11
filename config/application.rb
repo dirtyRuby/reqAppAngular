252ddcb561a8ccd75f45ae0836598b9d106d465a
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-03-11 15:58:28 +0200

    Assets pipeline.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ReqAppAngular
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
-    config.serve_static_assets = true
+    config.serve_static_files = true
     # Explicitly register the extensions we are interested in compiling
     config.assets.precompile.push(Proc.new do |path|
       File.extname(path).in? [
